e66242872ce7f2cc2441338a8c5694a67d995d2c
Anthony Woodward <two.woods@gmail.com>
2020-04-10 14:49:58 +0100

    [fixup] - work in progress commit

--- GameOfLife.Blazor/Pages/ConwaysBase.cs
+++ GameOfLife.Blazor/Pages/ConwaysBase.cs
@@ -3,66 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Blazor.Extensions.Canvas.Canvas2D;
-using Blazor.Extensions.Canvas;
-using Blazor.Extensions;
-using System.Drawing;
 using GameOfLife.Library;
+using BlazorSvgHelper.Classes.SubClasses;
+using BlazorSvgHelper;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace GameOfLife.BlazorUi.Pages
 {
     public class ConwaysBase : ComponentBase
     {
-        private Canvas2DContext _context;
-        protected BECanvasComponent _canvasReference;
-
         private readonly int cellPixelSize = 1115 / 140;
         readonly LifeGrid lifeGrid;
+
+        svg _Svg = null;
+
+        SvgHelper SvgHelper1 = new SvgHelper();
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            //ClockSettings.radius_2_Times = WidthAndHeight;
+            //ClockSettings.radius_Origin = WidthAndHeight / 2;
+            //ClockSettings.radius_90_Percent = ClockSettings.radius_Origin * 0.9;
+
+            //Generate_Clock_SVG();
+
+
+            SvgHelper1.Cmd_Render(_Svg, 0, builder);
+        }
 
         public ConwaysBase()
         {
             this.lifeGrid = new LifeGrid(gridHeight: 140, gridWidth: 223, CellFactory.GetCell());
             this.lifeGrid.Randomise();
-
-            // ShowGrid();
         }
 
-        private void ShowGrid()
-        {
-            // graphics.Clear(Color.White);
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            this._context = await this._canvasReference.CreateCanvas2DAsync();
-            //await this._context.SetFillStyleAsync("green");
-
-            //await this._context.FillRectAsync(10, 100, 100, 100);
-
-            //await this._context.SetFontAsync("48px serif");
-            //await this._context.StrokeTextAsync("Hello Blazor!!!", 10, 100);
-
-            int x = 0;
-            int y = 0;
-            int rowLength = this.lifeGrid.CurrentGrid.GetUpperBound(1) + 1;
-
-            foreach (Resident resident in this.lifeGrid.CurrentGrid)
-            {
-                if (resident.State == CellState.Alive)
-                {
-                    await this._context.SetFillStyleAsync("black");
-                    await this._context.FillRectAsync(x * cellPixelSize, y * cellPixelSize, cellPixelSize, cellPixelSize);
-                        //.FillRectangle(alive, x * cellPixelSize, y * cellPixelSize, cellPixelSize, cellPixelSize);
-                    //graphics.DrawRectangle(new Pen(Color.DarkGray), x * cellPixelSize, y * cellPixelSize, cellPixelSize, cellPixelSize);
-                }
-
-                x++;
-                if (x >= rowLength)
-                {
-                    x = 0;
-                    y++;
-                }
-            }
-        }
     }
 }
